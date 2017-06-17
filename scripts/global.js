72b877820bd82f9237e3921c48460464c135bdb2
ZD-ong <384474574@qq.com>
2017-06-17 13:30:32 +0800

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -29,3 +29,21 @@ function addClass(element,value){
 		element.className=newClassName;
 	}
 }
+
+function highlightPage(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	var headers= document.getElementsByTagName("header");
+	if(headers.length==0) return false;
+	var navs= headers[0].getElementsByTagName("nav");
+	if(navs.length==0) return false;
+	var links= navs[0].getElementsByTagName("a");
+	var linkurl;
+	for (var i=0;i<links.length;i++){
+		linkurl=links[i].getAttribute("href");
+		if(window.location.href.indexOf(linkurl)!= -1){
+			links[i].className="here";
+		}
+	}
+}
+addLoadEvent(highlightPage);
