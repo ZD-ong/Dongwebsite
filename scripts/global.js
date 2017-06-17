032e1bed05a4d6a2b6c6d74a67efe41aab7b2a3e
ZD-ong <384474574@qq.com>
2017-06-17 18:12:51 +0800

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -38,12 +38,52 @@ function highlightPage(){
 	var navs= headers[0].getElementsByTagName("nav");
 	if(navs.length==0) return false;
 	var links= navs[0].getElementsByTagName("a");
+	for (var i=0;i<links.length;i++){
 	var linkurl;
 	for (var i=0;i<links.length;i++){
 		linkurl=links[i].getAttribute("href");
 		if(window.location.href.indexOf(linkurl)!= -1){
 			links[i].className="here";
+			var linktext= links[i].lastChild.nodeValue.toLowerCase();
+			document.body.setAttribute("id",linktext);
+			}
 		}
 	}
 }
+
+//Experience
+function showSection(id){
+	var sections= document.getElementsByTagName("section");
+	for (var i=0;i<sections.length;i++){
+		if (sections[i].getAttribute("id")!=id){
+			sections[i].style.display="none";
+		}else{
+		 sections[i].style.display="block";
+		}
+	}
+}
+
+function prepareInternalnav(){
+	if (!document.getElementById) return false;
+	if (!document.getElementsByTagName) return false;
+	var articles= document.getElementsByTagName("article");
+	if (articles.length==0) return false;
+	var navs= articles[0].getElementsByTagName("nav");
+	if (navs.length==0) return false;
+	var nav= navs[0];
+	var links= nav.getElementsByTagName("a");
+	for (var i=0;i<links.length;i++){
+		var sectionId= links[i].getAttribute("href").split("#")[1];
+		if(!document.getElementById(sectionId)) continue;
+		document.getElementById(sectionId).style.display= "none";
+		links[i].destination= sectionId;
+		links[i].onclick= function(){
+			showSection(this.destination);
+			return false;
+		}
+	}
+}
+
+
 addLoadEvent(highlightPage);
+addLoadEvent(prepareInternalnav);
